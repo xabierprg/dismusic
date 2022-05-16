ad076eee2f50c630f6b2c6121bfe0802d92076eb
xabierprg <xabierpregolopez@gmail.com>
2022-05-17 00:46:07 +0200

    confirmar en "main"

--- src/index.py
+++ src/index.py
@@ -1,9 +1,10 @@
-from dis import dis
-from turtle import color
-import discord 
+import discord
 import datetime
+import re
+from urllib import parse, request
 from discord.ext import commands
-             
+
+
 bot = commands.Bot(command_prefix='-', description='Im a music player bot')
              
 # Commands
@@ -18,15 +19,35 @@ async def info(ctx):
                           timestamp=datetime.datetime.utcnow(), 
                           color=discord.Color.purple())
     
-    embed.add_field(name="Server Owner", value="xabierprg")
+    embed.add_field(name="Server Owner", value="xabierprg#5284")
     embed.add_field(name="GitHub", value="https://github.com/xabierprg/DisMusic")
     
     await ctx.send(embed=embed)
+    
+@bot.command()
+async def youtube(ctx, *, search):
+    query_string = parse.urlencode({'search_query': search})
+    html_content = request.urlopen('http://www.youtube.com/results?' + query_string)
+    search_results = re.findall('href=\"\\/watch\\?v=(.{11})', html_content.read().decode())
+    await ctx.send(search_results)
     
 # Events
 @bot.event
 async def on_ready():
     print('My bot is ready')
     
+# Read the Discord bot token and the owner id from the .properties file.
+# Creates two global variables (TOKEN and OWNER_ID).
+def read_properties():
+    global TOKEN
+    global OWNER_ID
+    
+    f = open("../.properties", "r")
+    lines = f.readlines()   
+    
+    TOKEN = lines[0].replace('\n', '').replace('TOKEN=', '')
+    OWNER_ID = lines[1].replace('\n', '').replace('OWNER_ID=', '')
+    
 
-bot.run('')
+read_properties()
+bot.run(TOKEN)
