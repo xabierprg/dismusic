dfc25d183ba6559bae71eae627ca195c46cf409b
xabierprg <xabierpregolopez@gmail.com>
2022-05-17 23:29:29 +0200

    connect bot to voice channel

--- src/index.py
+++ src/index.py
@@ -1,16 +1,17 @@
 import discord
 import datetime
-import re
-from urllib import parse, request
 from discord.ext import commands
+from youtubesearchpython import VideosSearch
 
 
 bot = commands.Bot(command_prefix='-', description='Im a music player bot')
+             
              
 # Commands
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
     
 @bot.command()
 async def info(ctx):
@@ -24,17 +25,21 @@ async def info(ctx):
     
     await ctx.send(embed=embed)
     
+
 @bot.command()
-async def youtube(ctx, *, search):
-    query_string = parse.urlencode({'search_query': search})
-    html_content = request.urlopen('http://www.youtube.com/results?' + query_string)
-    search_results = re.findall('href=\"\\/watch\\?v=(.{11})', html_content.read().decode())
-    await ctx.send(search_results)
+async def play(ctx):
+    await ctx.author.voice.channel.connect()
     
 # Events
 @bot.event
 async def on_ready():
-    print('My bot is ready')
+    print('Logged in as:\n{0.user.name}\n{0.user.id}'.format(bot))
+    
+    
+def search_video(search):
+    videosSearch = VideosSearch(search, limit = 1)
+    link = videosSearch.result().get('result')[0].get('link')
+    return link
     
 # Read the Discord bot token and the owner id from the .properties file.
 # Creates two global variables (TOKEN and OWNER_ID).
