44c109070d9d1f05f1c3dcc4f5f643cdbfe501ce
xabierprg <xabierpregolopez@gmail.com>
2022-05-23 23:26:51 +0200

    comments update

--- src/index.py
+++ src/index.py
@@ -4,17 +4,19 @@ from discord.ext import commands
 from player import Player
 
 
+# INITIALIZE MAIN OBJECTS
 bot = commands.Bot(command_prefix="-")
 player = Player(bot)
 
 
+"""Information about the bot usage."""
 @bot.command()
 async def info(ctx):
     embed = discord.Embed(
         title=f"DisMusic",
         description="Im a music player bot",
         timestamp=datetime.datetime.utcnow(),
-        color=discord.Color.purple(),
+        color=discord.Color.purple()
     )
 
     embed.add_field(name="Server Owner", value="xabierprg#5284", inline=False)
@@ -24,6 +26,7 @@ async def info(ctx):
     await ctx.send(embed=embed)
     
     
+"""Information about the bot commands."""
 @bot.command()
 async def commands(ctx):
     embed = discord.Embed(
@@ -39,58 +42,69 @@ async def commands(ctx):
     embed.add_field(name="-skip", value="skip a song in the queue", inline=False)
     embed.add_field(name="-shuffle", value="shuffle the queue", inline=False)
     embed.add_field(name="-show", value="show the songs in the queue", inline=False)
-    embed.add_field(name="-playing", value="show the playing track name", inline=False)
+    embed.add_field(name="-song", value="show the playing track name", inline=False)
     embed.add_field(name="-stop", value="stop the player and clear the queue", inline=False)
     embed.add_field(name="-clear", value="clear the queue", inline=False)
     
     await ctx.send(embed=embed)
 
 
+"""Launch event when the bot is ready."""
 @bot.event
 async def on_ready():
     print("Logged in as:\n{0.user.name}\n{0.user.id}".format(bot))    
 
+"""Play command."""
 @bot.command()
 async def play(ctx,* ,song):        
     await player.play_song(ctx, song)
     
+"""Add to the queue command."""
 @bot.command()
 async def add(ctx,* ,song):
     await player.add_to_queue(ctx, song)
     
+"""Pause command."""
 @bot.command()
 async def pause(ctx):
     await player.pause_song(ctx)
     
+"""Resume command."""
 @bot.command()
 async def resume(ctx):
     await player.resume_song(ctx)
     
+"""Skip command."""
 @bot.command()
 async def skip(ctx):
     await player.skip_song(ctx)
     
+"""Shuffle command."""
 @bot.command()
 async def shuffle(ctx):
     await player.shuffle_queue(ctx)
     
+"""Show command."""
 @bot.command()
 async def show(ctx):
     await player.show_queue(ctx)
-    
+
+"""Current song command."""
 @bot.command()
-async def playing(ctx):
+async def song(ctx):
     await player.show_playing_song(ctx)
     
+"""Stop command."""
 @bot.command()
 async def stop(ctx):
     await player.stop_player(ctx)
     
+"""Clear queue command."""
 @bot.command()
 async def clear(ctx):
     await player.clear_queue(ctx)
 
-
+"""This function read the .properties file with the bot tokens and ids"""
 def read_properties():
     global TOKEN
     global OWNER_ID
@@ -102,5 +116,6 @@ def read_properties():
     OWNER_ID = lines[1].replace("\n", "").replace("OWNER_ID=", "")
 
 
+#MAIN THREAD
 read_properties()
 bot.run(TOKEN)
--- src/player.py
+++ src/player.py
@@ -9,14 +9,18 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from discord import FFmpegPCMAudio
 from youtubesearchpython import VideosSearch
 
-FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
 
+"""Music player manager"""
 class Player():
+    
     def __init__(self, bot):
-        self.bot = bot
-        self.playing = ""
-        self.song_queue = queue.SimpleQueue()
+        self.bot = bot # bot main object
+        self.playing = "" # current playing song
+        self.song_queue = queue.SimpleQueue() # song queue variable
+        self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'} # ffmpeg options
         
+        
+    """This function read the .properties file with the spotify tokens and ids"""
     def read_properties(self):
         global CLIENT_ID
         global CLIENT_SECRET
@@ -28,12 +32,15 @@ class Player():
         CLIENT_SECRET = lines[3].replace("\n", "").replace("SPOTIFY_CLIENT_SECRET=", "")
         
             
+    """Search the video url from the input field"""
     def search_video(self, search):
-        self.playing = search
         videosSearch = VideosSearch(search, limit = 1)
         link = videosSearch.result().get('result')[0].get('link')
+        self.playing = videosSearch.result().get('result')[0].get('title')
         return link
     
+    
+    """Make a list with the names of the songs from the playlist"""
     def search_playlist(self, uri):
         self.read_properties()
         client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
@@ -44,26 +51,27 @@ class Player():
         for track in sp.playlist_tracks(playlist_URI)["items"]:
             track_name = track["track"]["name"]
             artist_name = track["track"]["artists"][0]["name"]
-            songlist.append(artist_name + " " + track_name)
+            songlist.append(artist_name + " - " + track_name)
         return songlist
     
     
+    """Loop that detects when the player isnt playing a song and has no paused songs"""
     async def loop(self, ctx):
         try:
             while ctx.voice_client.is_playing() or ctx.voice_client.is_paused():
                 await asyncio.sleep(3)
 
                 if ((not ctx.voice_client.is_playing()) and (not ctx.voice_client.is_paused())):
-                    print("NEXT")
                     await self.next_song(ctx)
                     return   
         except AttributeError:
             pass
     
             
+    """Play the song from the input field"""
     async def play_song(self, ctx, video): 
         if ctx.author.voice is None:
-            return await ctx.send("You must be in a voice channel to play a song.")
+            return await ctx.send("*You must be in a voice channel to play a song.*")
 
         if ctx.voice_client is None:
             await ctx.author.voice.channel.connect()
@@ -81,18 +89,18 @@ class Player():
             
         song = pafy.new(self.search_video(video))
         audio = song.getbestaudio()
-        source = FFmpegPCMAudio(source=audio.url, executable="../ffmpeg.exe", **FFMPEG_OPTIONS)
+        source = FFmpegPCMAudio(source=audio.url, executable="../ffmpeg.exe", before_options=self.FFMPEG_OPTIONS)
         ctx.voice_client.play(source)
         await self.loop(ctx)
                     
         
+    """Skip the current song detecting if the queue is empty"""
     async def next_song(self, ctx):
         if not self.song_queue.empty():
             await self.play_song(ctx, self.song_queue.get())
-        else:
-            await ctx.send("*The queue is empty*")
+            
         
-        
+    """Pause the current song"""
     async def pause_song(self, ctx):
         if not ctx.voice_client.is_playing():
             await ctx.send("*There is no song playing*")
@@ -102,7 +110,8 @@ class Player():
         else:
             await ctx.send("*The song is already paused*")
             
-            
+    
+    """Resume the paused song"""
     async def resume_song(self, ctx):
         if ctx.voice_client.is_paused():
             ctx.voice_client.resume()
@@ -110,19 +119,26 @@ class Player():
             await ctx.send("*The song is already resumed*")
             
             
+    """Skip the current song detecting if the song is playingor the queue is empty"""
     async def skip_song(self, ctx):
+        if self.song_queue.empty():
+            await ctx.send("*The queue is empty*")
+            return
+            
         if ctx.voice_client.is_playing():
             ctx.voice_client.pause()
         await self.next_song(ctx)
         
             
+    """Stop the player and disconnects the bot"""
     async def stop_player(self, ctx):
         if ctx.voice_client.is_connected():
             self.song_queue = queue.SimpleQueue()
             ctx.voice_client.stop()
             await ctx.voice_client.disconnect()
         
-            
+    
+    """Add a song or a playlist to the queue"""
     async def add_to_queue(self, ctx, song):
         if ctx.voice_client is None:
             await ctx.author.voice.channel.connect()
@@ -143,6 +159,7 @@ class Player():
                 await self.play_song(ctx, song)
         
         
+    """Shuffle the current queue"""
     async def shuffle_queue(self, ctx):
         song_list = []
         
@@ -156,11 +173,13 @@ class Player():
         [self.song_queue.put(i) for i in song_list]
         await ctx.send("*The queue was shuffled*")
         
-
+        
+    """Show the current playing song"""
     async def show_playing_song(self, ctx):
-        await ctx.send("**Playing:** " + self.playing)
+        await ctx.send("**Playing:**    " + self.playing)
         
-        
+    
+    """Show the current queue and playing song"""
     async def show_queue(self, ctx):
         song_list = ""
         songs = []
@@ -175,11 +194,15 @@ class Player():
         self.song_queue = queue.SimpleQueue()
         [self.song_queue.put(i) for i in songs]
         
-        for i in range(25):
-            if songs == len(songs):
-                oversize = False
-                break
-            song_list += songs[i]  + "\n"      
+        if not len(songs) == 0:
+            for i in range(25):
+                if i+1 == len(songs):
+                    oversize = False
+                    break
+                song_list += songs[i]  + "\n"
+        else:
+            song_list = "No track in queue"    
+            oversize = False
             
         songs.clear()
         
@@ -198,7 +221,8 @@ class Player():
         
         await ctx.send(embed=embed)
         
-            
+    
+    """Clear the current song queue"""
     async def clear_queue(self, ctx):
         self.song_queue = queue.SimpleQueue()
         await ctx.send("*The queue was cleared*")
