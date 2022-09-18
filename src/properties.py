878ad5656f5ae1ae4e73d274bd810454fcdbc5d5
xabierprg <xabierpregolopez@gmail.com>
2022-09-18 15:24:00 +0200

    properties change

--- src/properties.py
+++ src/properties.py
@@ -1,6 +1,6 @@
 class Properties:
 
-    TOKEN = "ODY0NjA1MTYyNjIyODEyMTcw.Gqw-cY.egXKo-nLNs2snB8Mmoa82qlIxbe9eJV84xiVKo"
+    TOKEN = "ODY0NjA1MTYyNjIyODEyMTcw.Gqw-cY.egXKo-nLNs2snB8Mmoa82qlIxbe9eJV84xiVKo..........."
     OWNER_ID = "294874766702018561"
     SPOTIFY_CLIENT_ID = "b7b62a008ee94c9184ec5084d261235e"
-    SPOTIFY_CLIENT_SECRET = "745c1d4dae274d47bcac9afd9479c289"
+    SPOTIFY_CLIENT_SECRET = "745c1d4dae274d47bcac9afd9479c289............."
